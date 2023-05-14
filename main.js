89939ae57d3a93dce29ba241fb8c009fca255cd9
Amlana-P <50160346+Amlana-P@users.noreply.github.com>
2023-05-14 12:44:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+let prevTrack = () => {
+    console.log("Inside prevTrack!")
+};
+
+let playPauseTrack = () => {
+    console.log("Inside playPausetrack!")
+};
+
+let nextTrack = () => {
+    console.log("Inside nextTrack!")
+};
+
+let seekTo = () => {
+    console.log("Inside seekTo!")
+};
+let setVolume = () => {
+    console.log("Inside setVolume!")
+};
